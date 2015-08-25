b8d7da4fea0d90eacdceb1219db7f6fc9697c8d7
mezzovide <reza.adrian@hanza.co.id>
2015-08-25 23:27:39 +0700

    update docs

--- supernet.js
+++ supernet.js
@@ -1,3 +1,4 @@
+
 /**
  * @apiDefine baseid
  * @apiParam {String} baseid Base Asset ID.
@@ -10,8 +11,14 @@
 
 /**
  * @apiDefine exchange
- * @apiParam {String="nxtae","InstantDEX"} exchange="null" (optional) If specified, only return data or use data from selected exchange. 
+ * @apiParam {String="nxtae","InstantDEX","bitfinex","btc38","bitstamp","btce","poloniex","bittrex","huobi","coinbase","okcoin","bityes","lakebtc","exmo","quadriga"} exchange="null" (optional) If specified, only return data or use data from selected exchange. 
  */
+
+/**
+ * @apiDefine ext_exchange
+ * @apiParam {String="bitfinex","btc38","bitstamp","btce","poloniex","bittrex","huobi","coinbase","okcoin","bityes","lakebtc","exmo","quadriga"} exchange Supported external exchanges
+ */
+
 
 /**
  * @apiDefine price
@@ -23,8 +30,8 @@
  * @apiParam {Number} volume Bid/Ask Volume.
  */
  
- /** 
- * @api {BitcoinDarkd SuperNET} '{"plugin":"InstantDEX","method":"allorderbooks"}' Allorderbooks
+/** 
+ * @api {btcd} '{"plugin":"InstantDEX","method":"allorderbooks"}' Allorderbooks
  * @apiVersion 0.1.0
  * @apiName getInstantDEXmethodAllorderbooks
  * @apiGroup InstantDEX
@@ -32,10 +39,10 @@
  * @apiParam {String} allorderbooks Get list of all active orderbooks.
  *
  *
- * @apiParamExample {btcd} Call-Example:
+ * @apiParamExample {btcd} RPC-Call-Example:
  * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"allorderbooks"}'
  *
- * @apiSuccessExample {json} Success-Response-Example:
+ * @apiSuccessExample {json} Response-Example:
  *{
  * "orderbooks": [
  *   {
@@ -58,10 +65,26 @@
  *}
  */
 
-
+/**
+ * @api {btcd} '{"plugin":"InstantDEX","method":"balance","exchange":"{exchange}"}' Balance
+ * @apiVersion 0.1.0
+ * @apiName GetInstantDEXMethodBalance
+ * @apiGroup InstantDEX
+ * @apiUse ext_exchange
+ *
+ * @apiParam {String} openorders Get list of active orders.
+ *
+ *
+ * @apiParamExample {btcd} RPC-Call-Example:
+ * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"balance","exchange":"btc38"}'
+ * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"balance","exchange":"poloniex"}'
+ *
+ * @apiSuccessExample {json} Response-Example:
+ * to be added
+ */
 
 /** 
- * @api {get} /InstantDEX?method=openorders&exchange={exchange} Openorders
+ * @api {btcd} '{"plugin":"InstantDEX","method":"openorders","exchange":"{exchange}"}' Openorders
  * @apiVersion 0.1.0
  * @apiName GetInstantDEXMethodOpenOrders
  * @apiGroup InstantDEX
@@ -69,17 +92,38 @@
  *
  * @apiParam {String} openorders Get list of active orders.
  *
- * @apiParamExample {curl} cURL-Example:
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=openorders" 
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=openorders&exchange=nxtae"	
  *
- * @apiParamExample {btcd} BitcoinDark-Example:
+ * @apiParamExample {btcd} RPC-Call-Example:
  * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"openorders"}'
  * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"openorders","exchange":"nxtae"}'
+ *
+ * @apiSuccessExample {json} Response-Example:
+ *{
+ * "openorders": [
+ *   {
+ *     "quoteid": "15812219587932742311",
+ *     "base": "Jay",
+ *     "baseid": "8688289798928624137",
+ *     "baseamount": "100000000",
+ *     "rel": "NXT",
+ *     "relid": "5527630",
+ *     "relamount": "100000000",
+ *     "price": 1,
+ *     "volume": 1,
+ *     "offerNXT": "6746683124695165774",
+ *     "timestamp": "1440498301",
+ *     "isask": "0",
+ *     "exchange": "InstantDEX",
+ *     "gui": ""
+ *   }
+ * ]
+ *}
+ *
+ *
  */
 
 /** 
- * @api {get} /InstantDEX?method=orderbook&baseid={baseid}&relid={relid}&exchange={exchange} Orderbook
+ * @api {btcd} '{"plugin":"InstantDEX","method":"openorders","baseid":"{baseid}","relid":"{relid}","exchange":"{exchange}"}' Orderbook
  * @apiVersion 0.1.0
  * @apiName GetInstantDEXMethodOrderbook
  * @apiGroup InstantDEX
@@ -89,24 +133,78 @@
  * 
  * @apiParam {String} orderbook Get orderbook for pairs.
  * 
- * @apiParamExample {curl} cURL-Example:
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=orderbook&baseid=8688289798928624137&relid=5527630"
- * #Open Jay/NXT orderbook from all exchanges combined.
- *
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=orderbook&baseid=8688289798928624137&relid=5527630&exchange=InstantDEX"
- * #Open Jay/NXT orderbook only from InstantDEX exchange.
- *
- * 
- * @apiParamExample {btcd} BitcoinDark-Example:
+ * @apiParamExample {btcd} RPC-Call-Example:
  * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"orderbook","baseid":"8688289798928624137","relid":"5527630"}'
  * #Open Jay/NXT orderbook from all exchanges combined.
  *
  * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"orderbook","baseid":"8688289798928624137","relid":"5527630","exchange":"InstantDEX"}'
  * #Open Jay/NXT orderbook only from InstantDEX exchange.
+ * 
+ * @apiSuccessExample {json} Response-Example:
+ *{
+ * "inverted": 0,
+ * "contract": "Jay/NXT",
+ * "baseid": "8688289798928624137",
+ * "relid": "5527630",
+ * "bids": [
+ *   {
+ *     "plugin": "InstantDEX",
+ *     "method": "tradesequence",
+ *     "dotrade": 1,
+ *     "price": 12.321,
+ *     "volume": 100
+ *   },
+ *   {
+ *     "plugin": "InstantDEX",
+ *     "method": "tradesequence",
+ *     "dotrade": 1,
+ *     "price": 12.32,
+ *     "volume": 99
+ *   },
+ *   {
+ *     "plugin": "InstantDEX",
+ *     "method": "tradesequence",
+ *     "dotrade": 1,
+ *     "price": 12.31,
+ *     "volume": 200
+ *   }
+ * ],
+ * "asks": [
+ *   {
+ *     "plugin": "InstantDEX",
+ *     "method": "tradesequence",
+ *     "dotrade": 1,
+ *     "price": 15.749,
+ *     "volume": 17
+ *   },
+ *   {
+ *     "plugin": "InstantDEX",
+ *     "method": "tradesequence",
+ *     "dotrade": 1,
+ *     "price": 15.75,
+ *     "volume": 225.0492
+ *   },
+ *   {
+ *     "plugin": "InstantDEX",
+ *     "method": "tradesequence",
+ *     "dotrade": 1,
+ *     "price": 15.79,
+ *     "volume": 149.948
+ *   }
+ * ],
+ * "numbids": 8,
+ * "numasks": 25,
+ * "lastbid": 12.321,
+ * "lastask": 15.749,
+ * "NXT": "6746683124695165774",
+ * "timestamp": 1440517635,
+ * "maxdepth": 25
+ *}
+ *
  */
 
 /**
- * @api {get} /InstantDEX?method=placeask&price={price}&volume={volume}&baseid={baseid}&relid={relid}&exchange={exchange} Placeask
+ * @api {btcd} '{"plugin":"InstantDEX","method":"placeask","baseid":"{baseid}","relid":"{relid}","price":"{price}","volume":"{volume}","exchange":"{exchange}"}' Placeask
  * @apiVersion 0.1.0
  * @apiName GetInstantDEXMethodPlaceAsk
  * @apiGroup InstantDEX
@@ -118,16 +216,38 @@
  *
  * @apiParam {String} placeask Submit ask order.
  * 
- * @apiParamExample {curl} cURL-Example:
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=placeask&price=15.5&volume=5&baseid=8688289798928624137&relid=5527630&exchange=InstantDEX"
- * #Submit ask order : 15 Jay assets @ 15.5 Nxt into InstantDEX.
+ * @apiParamExample {btcd} RPC-Call-Example:
+ * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"placeask","baseid":"8688289798928624137","relid":"5527630","price":1,"volume":1,"exchange":"InstantDEX"}'
+ * #Submit ask order : 1 Jay assets @ 1 Nxt into InstantDEX.
  *
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=placeask&price=15.5&volume=5&baseid=8688289798928624137&relid=5527630" 
- * #Submit ask order : 15 Jay assets @ 15.5 Nxt. Automatch will be used if no exchange specified.
+ * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"placeask","baseid":"8688289798928624137","relid":"5527630","price":1,"volume":1}'
+ * #Submit ask order : 1 Jay assets @ 1 Nxt. Automatch will be used if no exchange specified.
+ *
+ * @apiSuccessExample {json} Response-Example:
+ *{
+ * "quoteid": "17093329214979608856",
+ * "base": "Jay",
+ * "baseid": "8688289798928624137",
+ * "baseamount": "100000000",
+ * "rel": "NXT",
+ * "relid": "5527630",
+ * "relamount": "100000000",
+ * "price": 1,
+ * "volume": 1,
+ * "offerNXT": "6746683124695165774",
+ * "timestamp": "1440518266",
+ * "isask": "1",
+ * "exchange": "InstantDEX",
+ * "gui": "",
+ * "plugin": "relay",
+ * "destplugin": "InstantDEX",
+ * "method": "busdata",
+ * "submethod": "ask"
+ *}
  */
 
 /**
- * @api {get} /InstantDEX?method=placebid&price={price}&volume={volume}&baseid={baseid}&relid={relid}&exchange={exchange} Placebid
+ * @api {btcd} '{"plugin":"InstantDEX","method":"placebid","baseid":"{baseid}","relid":"{relid}","price":"{price}","volume":"{volume}","exchange":"{exchange}"}' Placebid
  * @apiVersion 0.1.0
  * @apiName GetInstantDEXMethodPlaceBid
  * @apiGroup InstantDEX
@@ -139,11 +259,35 @@
  *
  * @apiParam {String} placebid Submit bid order.
  *
- * @apiParamExample {curl} cURL-Example:
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=placebid&price=15.5&volume=5&baseid=8688289798928624137&relid=5527630&exchange=InstantDEX"
- * #Submit bid order : 15 Jay assets @ 15.5 Nxt into InstantDEX.
+ * @apiParamExample {btcd} RPC-Call-Example:
+ * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"placebid","baseid":"8688289798928624137","relid":"5527630","price":1,"volume":1,"exchange":"InstantDEX"}'
+ * #Submit bid order : 1 Jay assets @ 1 Nxt into InstantDEX.
  *
- * curl --url "http://127.0.0.1:7777/InstantDEX?method=placebid&price=15.5&volume=5&baseid=8688289798928624137&relid=5527630"
- * #Submit bid order : 15 Jay assets @ 15.5 Nxt. Automatch will be used if no exchange specified.
+ * ./BitcoinDarkd SuperNET '{"plugin":"InstantDEX","method":"placebid","baseid":"8688289798928624137","relid":"5527630","price":1,"volume":1}'
+ * #Submit bid order : 1 Jay assets @ 1 Nxt. Automatch will be used if no exchange specified.
+ *
+ * @apiSuccessExample {json} Response-Example:
+ *{
+ * "quoteid": "14436741218861091498",
+ * "base": "Jay",
+ * "baseid": "8688289798928624137",
+ * "baseamount": "100000000",
+ * "rel": "NXT",
+ * "relid": "5527630",
+ * "relamount": "100000000",
+ * "price": 1,
+ * "volume": 1,
+ * "offerNXT": "6746683124695165774",
+ * "timestamp": "1440518600",
+ * "isask": "0",
+ * "exchange": "InstantDEX",
+ * "gui": "",
+ * "plugin": "relay",
+ * "destplugin": "InstantDEX",
+ * "method": "busdata",
+ * "submethod": "bid"
+ *}
+ *
+ *
  */
-
+ 
